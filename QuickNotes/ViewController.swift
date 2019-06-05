beabf4ca5c4b57325b34d7e09a25a1c1dd833c2a
Kennon Ward <kennon.mward@gmail.com>
2019-06-05 12:04:31 -0400

    Initial Commit

---
+++ QuickNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickNotes
+//
+//  Created by Kennon Keys Ward on 6/5/19.
+//  Copyright © 2019 Kennon Keys Ward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
